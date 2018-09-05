e6e72afb5504c842599b0ba4c69cdaa146407217
Matt Green <mgreen14@wctc.edu>
2018-09-05 10:05:51 -0500

    SimpleServer Starter Code

---
+++ SimpleServlet/out/artifacts/SimpleServlet_war_exploded/index.jsp
@@ -0,0 +1,8 @@
+<html>
+<body>
+<h1>Simple Servlet</h1>
+<a href="simple?message='This is a message'">Simple Site</a>
+</body>
+
+
+</html>
---
+++ SimpleServlet/src/edu/wctc/simple/SimpleServlet.java
@@ -0,0 +1,29 @@
+package edu.wctc.simple;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "SimpleServlet")
+public class SimpleServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // Set response content type
+        response.setContentType("text/html");
+
+        // Actual logic goes here.
+        PrintWriter out = response.getWriter();
+        out.println("<html><head></head><body><h1>Simple Web Page</h1><p>The Test.");
+        out.println(request.getParameter("message"));
+        out.println("</p></body></html>");
+    }
+
+
+}
