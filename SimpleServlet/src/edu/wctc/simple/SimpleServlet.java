965a5bdd30cb513ee17676cf8646126bcfae9a66
Chandler <chandler.ryan@live.com>
2018-09-06 20:56:34 -0500

    Revert "Added the Week 3 project into the DistJava repository."
    This reverts commit 7e3b959a04655ed7a3ecbba099b4616319334b5a.

--- SimpleServlet/src/edu/wctc/simple/SimpleServlet.java
+++ SimpleServlet/src/edu/wctc/simple/SimpleServlet.java
@@ -7,32 +7,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.ArrayList;
 
 @WebServlet(name = "SimpleServlet")
 public class SimpleServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        Students theStudents2 = new Students();
-
-        ArrayList<Individual> result2;
-
-        result2 = theStudents2.findStudents(FindColumns.ACCTNUM, request.getParameter("message"));
-        PrintWriter out = response.getWriter();
-        out.println("<html><head></head><body><h1>Search results:</h1><p>");
-        for (Individual i: result2) {
-
-            out.println(i.getAccountNumber() + " " + i.getFname() + " " + i.getLname()+"<br>");
-        }
-
-
-
-        result2 = theStudents2.findStudents(FindColumns.LNAME, request.getParameter("message"));
-
-        for (Individual i: result2) {
-            out.println(i.getAccountNumber() + " " + i.getFname() + " " + i.getLname()+"<br>");
-        }
-        out.println("</p></body></html>");
-
 
     }
 
--- SimpleServlet/web/index.jsp
+++ SimpleServlet/web/index.jsp
@@ -1,12 +1,7 @@
 <html>
 <body>
-<h1>Simple Servlet Enter Search below:</h1>
-
-<form action="simple" method="post">
-    <label for="message">Search: </label>
-    <input name="message" id="message" />
-    <button type="submit" name="submit" id="submit" value="submit">Submit</button>
-</form>
+<h1>Simple Servlet</h1>
+<a href="simple?message=This is a message">Simple Site</a>
 </body>
 
 
